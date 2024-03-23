e60372f9103e1c8427d7998a156744cfc27b36bf
Hafizasad07 <hafizasadrafique@gmail.com>
2024-03-24 00:12:58 +0500

    upload

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Carousel
+jQuery(".carousel0").owlCarousel({
+    autoplay: true,
+    rewind: false,
+    loop: true,
+    /* use rewind if you don't want loop */
+    margin: 0, 
+    /*
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    nav: false,
+    dots: true,
+    items: 1,
+  });
