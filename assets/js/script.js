0a4e6dab616ec80fa02316a4fc974f506e02b992
Hafizasad07 <80450170+Hafizasad07@users.noreply.github.com>
2024-03-25 23:59:01 +0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,37 @@ jQuery(".carousel0").owlCarousel({
     dots: true,
     items: 1,
   });
+
+
+  function asTabs(node) {
+    var tabs = [];
+    for (var i = 0; i < node.childNodes.length; i++) {
+      var child = node.childNodes[i];
+      if (child.nodeType == document.ELEMENT_NODE)
+        tabs.push(child);
+    }
+    var tabList = document.createElement("div");
+    tabs.forEach(function(tab, i) {
+      var button = document.createElement("a");
+      console.log(button);
+      button.classList.add("data-tabname-a");
+      button.textContent = tab.getAttribute("data-tabname");
+      button.addEventListener("click", function() { selectTab(i); });
+      tabList.appendChild(button);
+    });
+    node.insertBefore(tabList, node.firstChild);
+    function selectTab(n) {
+      console.log(n);
+      $(".data-tabname-a").removeClass("active");
+      $($(".data-tabname-a")[n]).addClass("active")
+      tabs.forEach(function(tab, i) {
+        if (i == n)
+          tab.style.display = "block";
+        else
+          tab.style.display = "none";
+      });
+     
+    }
+    selectTab(0);
+  }
+  asTabs(document.querySelector("#tabwrapper"));
